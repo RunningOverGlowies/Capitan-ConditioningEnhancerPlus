ff7cecd63533d057a140c6420a716da49cdd62c7
capitan01R <hussainmoha95@gmail.com>
2026-01-11 06:30:03 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .enhancer import ConditioningEnhancer
+
+NODE_CLASS_MAPPINGS = {
+    "ConditioningEnhancer": ConditioningEnhancer,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ConditioningEnhancer": "Conditioning Enhancer (Qwen/Z-Image)",
+}
